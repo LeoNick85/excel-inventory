994568c76cb12a1eba66e1d3f2f831794c55ad40
Daniele Nicolini <dnicolini.fusiorari@gmail.com>
2020-12-02 15:25:00 +0100

    added-bootstrap-i

---
+++ resources/views/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bootstrap-italia.min.css">
+    <link rel="stylesheet" href="./css/app.css">
+    <title>Document</title>
+</head>
+<body class="container-fluid d-flex justify-content-center">
+    <h1 class="">Prova</h1>
+    
+    <script src="./js/bootstrap-italia.bundle.min.js"></script>    
+</body>
+</html>

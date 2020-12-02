c01e7491a032eb3b437aa06a0468b294ed2fd745
Daniele Nicolini <dnicolini.fusiorari@gmail.com>
2020-12-02 15:40:40 +0100

    vue-installed

---
+++ resources/js/views/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container-fluid blue d-flex justify-content-center">
+    <p class="p-5">{{message}}</p>
+  </div>
+</template>
+<script>
+const default_layout = "default";
+
+
+export default {
+  computed: {},
+  data() {
+      return {
+          message:'Hello World'
+      }
+  }
+};
+</script>
